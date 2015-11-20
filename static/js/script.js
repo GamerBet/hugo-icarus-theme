82c75af27f2b601f5a2c79c6b0ec0f5229b2fa1a
Collin Glass <collinglass@gmx.com>
2015-11-19 20:10:05 -0500

    merge parts of creative theme for main content pages

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,10 @@
-(function($){
+(function($) {
   var toTop = $('#toTop').length ? $('#toTop').offset().top - $(window).height() + 20 : 0;
 
   // Share
-  $('body').on('click', function(){
+  $('body').on('click', function() {
     $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
+  }).on('click', '.article-share-link', function(e) {
     e.stopPropagation();
 
     var $this = $(this),
@@ -13,23 +13,23 @@
       id = 'article-share-box-' + $this.attr('data-id'),
       offset = $this.offset();
 
-    if ($('#' + id).length){
+    if ($('#' + id).length) {
       var box = $('#' + id);
 
-      if (box.hasClass('on')){
+      if (box.hasClass('on')) {
         box.removeClass('on');
         return;
       }
     } else {
       var html = [
         '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
-          '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="fa fa-twitter article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="fa fa-facebook article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="fa fa-pinterest article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="fa fa-google article-share-google" target="_blank" title="Google+"></a>',
-          '</div>',
+        '<input class="article-share-input" value="' + url + '">',
+        '<div class="article-share-links">',
+        '<a href="https://twitter.com/intent/tweet?url=' + encodedUrl + '" class="fa fa-twitter article-share-twitter" target="_blank" title="Twitter"></a>',
+        '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="fa fa-facebook article-share-facebook" target="_blank" title="Facebook"></a>',
+        '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="fa fa-pinterest article-share-pinterest" target="_blank" title="Pinterest"></a>',
+        '<a href="https://plus.google.com/share?url=' + encodedUrl + '" class="fa fa-google article-share-google" target="_blank" title="Google+"></a>',
+        '</div>',
         '</div>'
       ].join('');
 
@@ -44,11 +44,11 @@
       top: offset.top + 25,
       left: offset.left
     }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
+  }).on('click', '.article-share-box', function(e) {
     e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
+  }).on('click', '.article-share-box-input', function() {
     $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
+  }).on('click', '.article-share-box-link', function(e) {
     e.preventDefault();
     e.stopPropagation();
 
@@ -56,8 +56,8 @@
   });
 
   // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
+  $('.article-entry').each(function(i) {
+    $(this).find('img').each(function() {
       if ($(this).parent().hasClass('fancybox')) return;
 
       var alt = this.alt;
@@ -67,29 +67,29 @@
       $(this).wrap('<a href="' + this.src + '" title="' + alt + '" class="fancybox"></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
+    $(this).find('.fancybox').each(function() {
       $(this).attr('rel', 'article' + i);
     });
   });
 
-  if ($.fancybox){
+  if ($.fancybox) {
     $('.fancybox').fancybox();
   }
 
   // Profile card
-  $(document).on('click', function () {
+  $(document).on('click', function() {
     $('#profile').removeClass('card');
-  }).on('click', '#profile-anchor', function (e) {
+  }).on('click', '#profile-anchor', function(e) {
     e.stopPropagation();
     $('#profile').toggleClass('card');
-  }).on('click', '.profile-inner', function (e) {
+  }).on('click', '.profile-inner', function(e) {
     e.stopPropagation();
   });
 
   // To Top
-  $(document).on('scroll', function () {
+  $(document).on('scroll', function() {
     if ($(document).width() >= 800) {
-      if($(this).scrollTop() > toTop) {
+      if ($(this).scrollTop() > toTop) {
         $('#toTop').addClass('fix');
         $('#toTop').css('left', $('#sidebar').offset().left);
       } else {
@@ -99,8 +99,29 @@
       $('#toTop').addClass('fix');
       $('#toTop').css('right', 20);
     }
-  }).on('click', '#toTop', function () {
+  }).on('click', '#toTop', function() {
     $(document).scrollTop(0);
+  });
+
+  $(".submitGetReplays").click(function(event) {
+    $("#getReplays").submit();
+    event.preventDefault();
+  });
+
+  $("#getReplays").submit(function(event) {
+    var data = {}
+    data.summonerName = $("#summonerName").val();
+    data.region = $("#region").val();
+    data.email = $("#email").val();
+
+    $.ajax({
+      type: "POST",
+      url: "https://gamerbet.co/api/ec/contact",
+      data: data,
+      success: console.log,
+      dataType: "json"
+    });
+    event.preventDefault();
   });
 
 })(jQuery);
